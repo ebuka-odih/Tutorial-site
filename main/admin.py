e348fd59ab46f9c3d76b2cf2745092c8ae85e4be
Emmanuel <emmagab38@gmail.com>
2019-03-25 17:41:09 +0100

    Second commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . models import Tutorial
+from tinymce.widgets import TinyMCE
+from django.db import models
 
-# Register your models here.
+
+#  this is a way you can change the order of how your models shows on the Admin
+class TutorialAdmin(admin.ModelAdmin):
+    # fields = ["tutorial_title", "tutorial_content", "tutorial_published"]
+    fieldsets = [
+        ('Title/date', {"fields": ["tutorial_title", "tutorial_published"]}),
+        ("Content", {"fields": ["tutorial_content"]})
+    ]
+    formfield_overide = { 
+        models.TextField: {'widgets': TinyMCE()}
+    }
+
+admin.site.register(Tutorial, TutorialAdmin)
